1b3dde2afac18abd11f09256956f1d09912820fb
William <wvuong3@myseneca.ca>
2021-10-12 15:52:10 -0400

    register user and add to mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check')
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/Users');
 
@@ -12,12 +13,40 @@ router.post('/', [
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more charaters').isLength({ min: 6 })
 ], 
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed');
+    
+    const { name, email, password } = req.body;
+    try{
+        let user = await User.findOne({ email });
+        
+        if(user) {
+            return res.status(400).json({ msg: 'User already exists' })
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('user saved');
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error')
+    }
+    
+
 });
 
 module.exports = router;
