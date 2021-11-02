5a7496eba76b7bf76b6228d48371bfc018a4d47b
William <wvuong3@myseneca.ca>
2021-11-02 13:18:59 -0400

    authentication

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,19 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
 const User = require('../models/Users');
+const auth = require('../middleware/auth.js');
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @@access Private
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
 });
 
 // @route   POST api/auth
