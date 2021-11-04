fa8954737ac72ae6af79a945d87f87f7e8782eb2
William <wvuong3@myseneca.ca>
2021-11-04 17:06:38 -0400

    added navigation and pages, and changed react-router-dom version

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className="App">
-      My app
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+        </Switch>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
